4b4fd9a4513b66d1d15bd4478f04dca6553c9d66
Matthew <logicprojectsforfpgas@gmail.com>
2023-06-06 14:55:38 -0600

    basic grid system outline

---
+++ src/grid.rs
@@ -0,0 +1,71 @@
+use std::marker::PhantomData;
+
+use bevy::prelude::*;
+
+pub const GRID_SIZE: usize = 48;
+
+// TODO impl iterator
+#[derive(Resource)]
+pub struct Grid<T> {
+    // Problems:
+    // Entites may be despawned and would leave bad refrence - handled by removed system
+    // Entities may have never been added to the right grid - handled by add system
+    pub entities: [[Option<Entity>; GRID_SIZE]; GRID_SIZE],
+    _marker: PhantomData<T>,
+}
+
+impl<T> Default for Grid<T> {
+    fn default() -> Self {
+        Self {
+            entities: [[None; GRID_SIZE]; GRID_SIZE],
+            _marker: Default::default(),
+        }
+    }
+}
+
+fn remove_from_grid<T: Component>(mut grid: ResMut<Grid<T>>, mut query: RemovedComponents<T>) {
+    for removed_entity in query.iter() {
+        // Search for entity
+        if let Some(entity) = grid
+            .entities
+            .iter_mut()
+            .flatten()
+            .filter(|entity| entity.is_some())
+            .find(|entity| entity.unwrap() == removed_entity)
+        {
+            *entity = None;
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct GridLocation {
+    pub position: (usize, usize),
+}
+
+fn add_to_grid<T: Component>(
+    mut grid: ResMut<Grid<T>>,
+    query: Query<(Entity, &GridLocation), Added<T>>,
+) {
+    for (entity, location) in &query {
+        if let Some(existing) = grid.entities[location.position.0][location.position.1] {
+            if existing != entity {
+                grid.entities[location.position.0][location.position.1] = Some(entity);
+            }
+        } else {
+            grid.entities[location.position.0][location.position.1] = Some(entity);
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct GridPlugin<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T: Component> Plugin for GridPlugin<T> {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<Grid<T>>()
+            .add_systems(PreUpdate, (add_to_grid::<T>, remove_from_grid::<T>));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,10 @@
-use std::marker::PhantomData;
-
 use bevy::{prelude::*, window::PresentMode};
 
 pub const WIDTH: f32 = 640.0;
 pub const HEIGHT: f32 = 480.0;
-pub const GRID_SIZE: usize = 48;
 
-// TODO impl iterator
-#[derive(Resource)]
-pub struct Grid<T> {
-    // Problems:
-    // Entites may be despawned and would leave bad refrence
-    // Entities may have never been added to the right grid
-    entities: [[Option<Entity>; GRID_SIZE]; GRID_SIZE],
-    _marker: PhantomData<T>,
-}
-
-impl<T> Default for Grid<T> {
-    fn default() -> Self {
-        Self {
-            entities: [[None; GRID_SIZE]; GRID_SIZE],
-            _marker: Default::default(),
-        }
-    }
-}
-
-fn remove_from_grid<T: Component>(mut grid: ResMut<Grid<T>>, query: Query<&T>) {
-    for entity in grid
-        .entities
-        .iter_mut()
-        .flatten()
-        .filter(|entity| entity.is_some())
-    {
-        if !query.contains(entity.unwrap()) {
-            *entity = None;
-        }
-    }
-}
-
-fn react_on_removal(removed: RemovedComponents<MyComponent>, mut query: Query<&mut Sprite>) {
-    // `RemovedComponents<T>::iter()` returns an interator with the `Entity`s that had their
-    // `Component` `T` (in this case `MyComponent`) removed at some point earlier during the frame.
-    for entity in removed.iter() {
-        if let Ok(mut sprite) = query.get_mut(entity) {
-            sprite.color.set_r(0.0);
-        }
-    }
-}
-
-#[derive(Component)]
-pub struct GridLocation {
-    position: (usize, usize),
-}
-
-fn add_to_grid<T: Component>(
-    mut grid: ResMut<Grid<T>>,
-    query: Query<(Entity, &GridLocation), With<T>>,
-) {
-    for (entity, location) in &query {
-        if let Some(existing) = grid.entities[location.position.0][location.position.1] {
-            if existing != entity {
-                grid.entities[location.position.0][location.position.1] = Some(entity);
-            }
-        } else {
-            grid.entities[location.position.0][location.position.1] = Some(entity);
-        }
-    }
-}
+mod grid;
+use grid::{Grid, GridLocation, GridPlugin};
 
 // Is default really required
 #[derive(Component, Default, Debug)]
@@ -74,7 +12,12 @@ pub struct Wall {
     health: f32,
 }
 
-fn use_grid(grid: Res<Grid<Wall>>, walls: Query<&Wall>) {
+fn use_grid(
+    grid: Res<Grid<Wall>>,
+    walls: Query<&Wall>,
+    keyboard: Res<Input<KeyCode>>,
+    mut commands: Commands,
+) {
     for entity in grid
         .entities
         .iter()
@@ -87,18 +30,9 @@ fn use_grid(grid: Res<Grid<Wall>>, walls: Query<&Wall>) {
             .get(*entity)
             .expect("entity in grid does not have wall component");
         info!("{:?}", wall);
-    }
-}
-
-#[derive(Default)]
-pub struct GridPlugin<T> {
-    _marker: PhantomData<T>,
-}
-
-impl<T: Component> Plugin for GridPlugin<T> {
-    fn build(&self, app: &mut App) {
-        app.init_resource::<Grid<T>>()
-            .add_systems(Update, (add_to_grid::<T>, remove_from_grid::<T>));
+        if keyboard.just_pressed(KeyCode::A) {
+            commands.entity(*entity).despawn();
+        }
     }
 }
 
