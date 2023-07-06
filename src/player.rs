f8bc8c09001cfdde654a36280e2ff8c05706e0b3
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-06 15:45:16 -0600

    build controls

--- src/player.rs
+++ src/player.rs
@@ -4,18 +4,39 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
+        app.init_resource::<ClickMode>().add_systems(
             Update,
-            (left_click_to_build_walls, right_click_to_remove_walls),
+            (left_click_to_build, right_click_to_remove, set_build_mode),
         );
     }
 }
 
-fn left_click_to_build_walls(
+#[derive(Default, Resource)]
+enum ClickMode {
+    None,
+    BuildWall,
+    #[default]
+    BuildFoodMachine,
+}
+
+fn set_build_mode(keyboard: Res<Input<KeyCode>>, mut mode: ResMut<ClickMode>) {
+    if keyboard.just_pressed(KeyCode::Key1) {
+        *mode = ClickMode::None;
+    }
+    if keyboard.just_pressed(KeyCode::Key2) {
+        *mode = ClickMode::BuildWall;
+    }
+    if keyboard.just_pressed(KeyCode::Key3) {
+        *mode = ClickMode::BuildFoodMachine;
+    }
+}
+
+fn left_click_to_build(
     mut commands: Commands,
     wall_grid: Res<Grid<Wall>>,
     cursor_position: Res<CursorPosition>,
     mouse: Res<Input<MouseButton>>,
+    mode: Res<ClickMode>,
 ) {
     if !mouse.pressed(MouseButton::Left) {
         return;
@@ -25,17 +46,35 @@ fn left_click_to_build_walls(
         if wall_grid.occupied(&location) {
             return;
         }
-        commands.spawn((
-            SpatialBundle::default(),
-            Wall { _health: 10.0 },
-            LockToGrid,
-            WallSprite::None,
-            location,
-        ));
+        match mode.as_ref() {
+            ClickMode::None => {}
+            ClickMode::BuildWall => {
+                commands.spawn((
+                    SpatialBundle::default(),
+                    Wall { _health: 10.0 },
+                    LockToGrid,
+                    WallSprite::None,
+                    location,
+                ));
+            }
+            ClickMode::BuildFoodMachine => {
+                commands.spawn((
+                    SpatialBundle::default(),
+                    Machine {
+                        use_offset: IVec2 { x: 0, y: -1 },
+                    },
+                    FoodMachine { rate: 10.0 },
+                    LockToGrid,
+                    MachineSprite::FoodMachine,
+                    Wall { _health: 10.0 },
+                    location,
+                ));
+            }
+        }
     }
 }
 
-fn right_click_to_remove_walls(
+fn right_click_to_remove(
     mut commands: Commands,
     wall_grid: Res<Grid<Wall>>,
     cursor_position: Res<CursorPosition>,
